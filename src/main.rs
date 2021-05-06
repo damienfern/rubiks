58943776d8d239a6d5dc6ebf19bf099253f12166
Damien Fernandes <damienfern@users.noreply.github.com>
2021-05-06 12:39:04 +0200

    testing some changes

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ enum Color {
     Green
 }
 
+#[derive(Debug, Clone, Copy)]
 enum Move {
     U,
     UPrime,
@@ -37,6 +38,7 @@ struct Face {
 }
 
 impl Face {
+    #[warn(dead_code)]
     fn new(top_left: Color, top: Color, top_right: Color, center_left: Color, center: Color, center_right: Color, bottom_left: Color, bottom: Color, bottom_right: Color) -> Face {
         Face {
             top_left, top, top_right, center_left, center, center_right, bottom_left, bottom, bottom_right
@@ -47,6 +49,20 @@ impl Face {
             top_left: color, top: color, top_right: color, center_left: color, center: color, center_right: color, bottom_left: color, bottom: color, bottom_right: color
         }
     }
+    
+    fn set_color (&mut self, new_color: Color) {
+        self.top_left = new_color;
+    }
+}
+
+#[derive(Debug)]
+enum TypeFace {
+    Front,
+    Left,
+    Right,
+    Back,
+    Down,
+    Up
 }
 
 #[derive(Debug)]
@@ -60,8 +76,8 @@ struct Cube {
 }
 
 impl Cube {
-    fn new (front: Face, left: Face, right: Face, back: Face, down: Face, up: Face) -> Cube {
-        Cube {
+    fn new (front: Face, left: Face, right: Face, back: Face, down: Face, up: Face) -> Self {
+        Self {
             front,
             left,
             right,
@@ -69,6 +85,19 @@ impl Cube {
             down,
             up
         }
+    }
+    fn execute_move (&self, type_face: TypeFace, move_to_execute: Move) {
+        // face.top_left = Color::Yellow;
+        let face = match type_face {
+            TypeFace::Front => &self.front,
+            TypeFace::Back => &self.back,
+            TypeFace::Left => &self.left,
+            TypeFace::Right => &self.right,
+            TypeFace::Up => &self.up,
+            TypeFace::Down => &self.down,
+        };
+        face.set_color(Color::Yellow);
+        println!("{:?}, {:?}", face, move_to_execute);
     }
 }
 
@@ -79,8 +108,10 @@ fn main() {
     let back_face = Face::new_completed(Color::Orange);
     let down_face = Face::new_completed(Color::Green);
     let up_face = Face::new_completed(Color::Blue);
-
+    
     let cube = Cube::new(front_face, left_face, right_face, back_face, down_face, up_face);
-
-    println!("{:?}", cube);
+    cube.execute_move(TypeFace::Front, Move::U);
+    cube.execute_move(TypeFace::Left, Move::L);
+    cube.execute_move(TypeFace::Right, Move::RPrime);
+    // println!("{:?}", cube);
 }
